20e081a0c6947ee9327b96cede727200698dac85
Rajasekhar Komma <Rajasekhar Komma@DESKTOP-BKH1E4P>
2022-03-15 13:12:13 +0530

    Commit

---
+++ src/test/java/ContactTest/CreateContactTest.java
@@ -0,0 +1,17 @@
+package ContactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest 
+{
+	@Test
+public void createContactTest()
+{
+System.out.println("this is ContactTest");
+}
+	@Test
+	public void createContactTset1()
+	{
+		System.out.println("This is contact tset2");
+	}
+}
---
+++ src/test/java/OrgTest/CreateOrgTest.java
@@ -0,0 +1,12 @@
+package OrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest 
+{
+	@Test
+public void createOrgTest() 
+{
+System.out.println("this is OrgTest");	
+}
+}
